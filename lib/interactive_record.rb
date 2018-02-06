b926081a85620c5341a1faf54285a6d94fbb64c6
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 20:20:48 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,7 +26,7 @@ class InteractiveRecord
     column_names.compact
   end
 
-  def table_name_for_insert
+  def col_name_for_insert
     self.class.column_names.delete_if { |col| col == "id" }.join(", ")
   end
 end
