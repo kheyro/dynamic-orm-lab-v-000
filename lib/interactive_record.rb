a36b2ed6aa541cfd3d4ebb3cf08410fe738706af
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 20:34:47 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,4 +52,10 @@ class InteractiveRecord
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
+
+  def self.find_by(hash)
+    key, value = hash.first
+    sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'"
+    DB[:conn].execute(sql)
+  end
 end
